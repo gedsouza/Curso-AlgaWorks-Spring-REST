e854f4e7fb428f8267960e59b3947512abdd246e
gedsouza <geovanne_paes@hotmail.com>
2022-01-13 00:45:17 -0300

    commit aula 1

---
+++ geovalog-api/src/main/java/com/algaworks/geovalog/GeovalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.geovalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeovalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeovalogApiApplication.class, args);
+	}
+
+}
